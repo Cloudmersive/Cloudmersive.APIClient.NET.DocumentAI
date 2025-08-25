97837bd46e9896b6e29e9612998a43e70eb03ae2
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-08-25 00:45:30 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
 ﻿Remove-Item –path ./client –recurse
-& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/barcode -l csharp -o client -c packageconfig.json
+
+Invoke-WebRequest -Uri 'https://api.cloudmersive.com/document-ai/docs/v1/swagger' -OutFile '.\documentai-api-swagger.json'
+(Get-Content .\documentai-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\documentai-api-swagger.json
+(Get-Content .\documentai-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\documentai-api-swagger.json -Encoding UTF8
+
+& java -jar swagger-codegen-cli-2.4.5.jar generate -i .\documentai-api-swagger.json -l csharp -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 #& npm build ./client
